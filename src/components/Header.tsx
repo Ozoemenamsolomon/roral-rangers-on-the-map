6f5746e8d6b685420a9c296e2b94e3fcf1cdd01c
Ozoemenamsolomon <ozoemenamsolomon@yahoo.com>
2021-06-23 19:08:04 +0200

    Rename Layout from DesktopLayout, remove browser default style, create container component, worked a bit on responsiveness, add useEffect useState  for resize and it works so fine

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,84 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Container from './Container';
 
-export interface HeaderProps {}
+export interface HeaderProps {
+  screenWidth: number;
+}
 
-const Header = () => {
+const Header: React.FC<HeaderProps> = ({ screenWidth }) => {
   return (
-    <header>
-      <BrandLink to="/">
-        <h1>Hello from Header Page</h1>
-      </BrandLink>
-      <nav>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/shop">Shop</NavLink>
-        <NavLink to="/docs">Docs</NavLink>
-      </nav>
-    </header>
+    <MyHeader>
+      <Container
+        display="flex"
+        justifyContent={screenWidth > 600 ? 'space-between' : 'center'}
+        alignItems="center"
+        width={90}
+      >
+        <BrandLink to="/">
+          <h1>Header Logo</h1>
+        </BrandLink>
+        <NavBar>
+          <NavList>
+            <NavItem>
+              <NavLink to="/about">
+                <span>About</span>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/shop">
+                <span>Shop</span>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/docs">
+                <span>Docs</span>
+              </NavLink>
+            </NavItem>
+          </NavList>
+        </NavBar>
+      </Container>
+    </MyHeader>
   );
 };
 
 export default Header;
 
-const NavLink = styled(Link)`
-  color: #000;
+const MyHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+`;
+const NavList = styled.ul`
+  justify-content: space-around;
+  display: flex;
+`;
+const NavItem = styled.li`
+  list-style: none;
 `;
 
-const BrandLink = styled(NavLink)``;
+const NavLink = styled(Link)`
+  color: #000;
+  text-decoration: none;
+  height: 100%;
+  padding: 0 1.5em;
+`;
+
+const BrandLink = styled(NavLink)`
+  padding: 1em 0.5em;
+`;
+
+const NavBar = styled.nav`
+  @media (max-width: 600px) {
+    & {
+      bottom: 0;
+      position: fixed;
+      background-color: bisque;
+      width: 100%;
+      right: 0;
+    }
+  }
+`;
